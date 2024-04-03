2ff4a8293723debe76d3dbab1f8b8c43c50f67f0
Faddli Lindra Wibowo <faddli.lindra.wibowo@gmail.com>
2024-04-04 03:36:39 +0700

    path

--- PalmOil_Detection.py
+++ PalmOil_Detection.py
@@ -8,12 +8,7 @@ from PIL import Image
 
 @st.cache_resource
 def load_model():
-    # model = torch.hub.load('ultralytics/yolov5','custom',path="weights/best.pt",force_reload=True)
-    # model = torch.hub.load("ultralytics/yolov5", 'custom', path='weights/best.pt', source='local') 
-
-
-    # model = torch.hub.load('ultralytics/yolov5', 'custom', path='weights/best.pt')  # local model
-    model = torch.hub.load('./yolov5', 'custom', path='weights/best.pt', source='local')  # local repo
+    model = torch.hub.load('ultralytics/yolov5','custom',path="weights/best.pt",force_reload=True)
     return model
 
 demo_img = "palm-oil.png"
